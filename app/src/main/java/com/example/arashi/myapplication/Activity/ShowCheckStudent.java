443b0f26c30b2835fe09896458d9cdda4e9f9684
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2016-05-08 22:01:39 +0700

    Student Present Absent

--- app/src/main/java/com/example/arashi/myapplication/Activity/ShowCheckStudent.java
+++ app/src/main/java/com/example/arashi/myapplication/Activity/ShowCheckStudent.java
@@ -2,7 +2,12 @@ package com.example.arashi.myapplication.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 
 import com.example.arashi.myapplication.Object.Class;
@@ -19,10 +24,21 @@ public class ShowCheckStudent extends AppCompatActivity {
     ClassLocalStore classLocalStore;
     SeverRequests severRequests;
     Class classroom;
+    Button presentButton,absentButton;
+    TableRow tableTextHead;
+    LinearLayout down;
+    TextView presentText,absentText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_check_student);
+
+        absentButton = (Button) findViewById(R.id.absentButton);
+        presentButton = (Button) findViewById(R.id.presentButton);
+        tableTextHead = (TableRow) findViewById(R.id.tableTextHead);
+        down = (LinearLayout) findViewById(R.id.down);
+        presentText = (TextView) findViewById(R.id.presentText);
+        absentText = (TextView) findViewById(R.id.absentText);
 
         listView = (ListView) findViewById(R.id.listView1);
         listItem = new ArrayList<>();
@@ -33,6 +49,12 @@ public class ShowCheckStudent extends AppCompatActivity {
         listView.setAdapter(adapter);
         classroom = classLocalStore.getJoinedInClass();
 
+        tableTextHead.setVisibility(View.INVISIBLE);
+        down.setVisibility(View.INVISIBLE);
+        presentText.setVisibility(View.INVISIBLE);
+        absentText.setVisibility(View.INVISIBLE);
+
+
         severRequests.showCheckedStudentListInBackground(classroom, new GetShowCheckedStudent() {
             @Override
             public void done(ArrayList<Roster> returnedListRoster) {
@@ -42,5 +64,24 @@ public class ShowCheckStudent extends AppCompatActivity {
                 }
             }
         });
+        presentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tableTextHead.setVisibility(View.VISIBLE);
+                down.setVisibility(View.VISIBLE);
+                presentText.setVisibility(View.VISIBLE);
+                absentText.setVisibility(View.GONE);
+            }
+        });
+        absentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tableTextHead.setVisibility(View.VISIBLE);
+                down.setVisibility(View.VISIBLE);
+                presentText.setVisibility(View.GONE);
+                absentText.setVisibility(View.VISIBLE);
+            }
+        });
+
     }
 }
