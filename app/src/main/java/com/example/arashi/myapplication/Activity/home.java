f9248cb4b6bc06920c613dbf447a41ab07855636
PunnawatWattanapanupat <gameineer@gmail.com>
2016-05-16 12:56:36 +0700

    update_file_fix_bugs_add_notice_question_16/6/2016

--- app/src/main/java/com/example/arashi/myapplication/Activity/home.java
+++ app/src/main/java/com/example/arashi/myapplication/Activity/home.java
@@ -104,6 +104,7 @@ public class home extends Activity implements View.OnClickListener  {
         }
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
